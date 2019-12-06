f9868dc7981d48dfa21463845fce4a5fc4f94751
Your Name <you@example.com>
2019-12-06 18:03:29 +0800

    Updates

---
+++ precache-manifest.b04b2cba4c6b0e1de339107b6f3eb768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fe9759bb03d0fca5a060a460443283",
+    "url": "/points/index.html"
+  },
+  {
+    "revision": "9f2f6e9902e6029ebf6d",
+    "url": "/points/static/css/main.654841fc.chunk.css"
+  },
+  {
+    "revision": "aec5c2c41ba873411092",
+    "url": "/points/static/js/2.f6c22d89.chunk.js"
+  },
+  {
+    "revision": "9f2f6e9902e6029ebf6d",
+    "url": "/points/static/js/main.8fbd392b.chunk.js"
+  },
+  {
+    "revision": "61152c3d88e85b08b81d",
+    "url": "/points/static/js/runtime-main.1b59a89c.js"
+  },
+  {
+    "revision": "402c0c3f9ad95557355234c4721de230",
+    "url": "/points/static/media/Adobe+Garamond+Pro+Regular.402c0c3f.ttf"
+  }
+]);
